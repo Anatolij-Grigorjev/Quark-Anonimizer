e1b67535cf89e47e47536838df513bc744117b75
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-11-11 22:12:54 +0200

    add config and test for local MySQL connection

--- src/main/java/tiem625/GreetingResource.java
+++ src/main/java/tiem625/GreetingResource.java
@@ -1,16 +1,23 @@
 package tiem625;
 
+import io.agroal.api.AgroalDataSource;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.sql.SQLException;
+
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    private AgroalDataSource dataSource;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public String hello() throws SQLException {
+        return "Mysql connection OK: " + dataSource.isHealthy(true);
     }
 }
