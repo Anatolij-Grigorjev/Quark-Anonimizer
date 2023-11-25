34a7c052bfe889fc34c2cf376a26b371d7985867
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-11-25 18:23:26 +0200

    remove sampler method from generator interface - will be separate concern

--- src/main/java/tiem625/anonimizer/generating/DataGenerator.java
+++ src/main/java/tiem625/anonimizer/generating/DataGenerator.java
@@ -1,16 +1,16 @@
 package tiem625.anonimizer.generating;
 
 import jakarta.annotation.Nonnull;
-import tiem625.anonimizer.DataObject;
-import tiem625.anonimizer.commonterms.*;
+import tiem625.anonimizer.commonterms.Amount;
+import tiem625.anonimizer.commonterms.BatchName;
+import tiem625.anonimizer.commonterms.FieldName;
+import tiem625.anonimizer.commonterms.FieldType;
 
 import java.util.List;
 
 public interface DataGenerator {
 
     void generate(@Nonnull DataGenerationRules rules);
-
-    List<DataObject> fetchSample(@Nonnull SampleSize desiredSize);
 
     class DataGenerationRules {
         BatchName batchName;
