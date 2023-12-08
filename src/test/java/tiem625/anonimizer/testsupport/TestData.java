aa975eb8e23550c4aaa62df16cd92aaf8e2bf18f
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-12-08 23:22:26 +0200

    add more helper methods

--- src/test/java/tiem625/anonimizer/testsupport/TestData.java
+++ src/test/java/tiem625/anonimizer/testsupport/TestData.java
@@ -1,11 +1,19 @@
 package tiem625.anonimizer.testsupport;
 
+import tiem625.anonimizer.commonterms.Amount;
 import tiem625.anonimizer.commonterms.BatchName;
 import tiem625.anonimizer.commonterms.FieldName;
 import tiem625.anonimizer.commonterms.FieldType;
 import tiem625.anonimizer.generating.DataGenerator.DataFieldSpec;
+import tiem625.anonimizer.generating.DataGenerator.DataGenerationRules;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class TestData {
+
+    private final Random RNG = new Random();
 
     public TestData() {
     }
@@ -15,4 +23,27 @@ public class TestData {
     public DataFieldSpec textFieldSpec(String name) {
         return new DataFieldSpec(FieldName.of(name), FieldType.TEXT);
     }
+
+    public Amount randomAmount() {
+        return Amount.of(RNG.nextInt(1, 15));
+    }
+
+    public List<DataFieldSpec> fieldSpecsList(Map<String, FieldType> fieldSpecsMap) {
+        return fieldSpecsMap.entrySet().stream()
+                .map(entry -> new DataFieldSpec(FieldName.of(entry.getKey()), entry.getValue()))
+                .toList();
+    }
+
+    public DataGenerationRules dataGenerationRules(Map<String, FieldType> fieldSpecsMap) {
+        return dataGenerationRules(TST_BATCH, fieldSpecsMap, randomAmount());
+    }
+
+    public DataGenerationRules dataGenerationRules(String batchName, Map<String, FieldType> fieldSpecsMap, int amount) {
+        return dataGenerationRules(BatchName.of(batchName), fieldSpecsMap, Amount.of(amount));
+    }
+
+    public DataGenerationRules dataGenerationRules(BatchName batchName, Map<String, FieldType> fieldSpecsMap, Amount amount) {
+        return new DataGenerationRules(batchName, fieldSpecsList(fieldSpecsMap), amount);
+    }
+
 }
