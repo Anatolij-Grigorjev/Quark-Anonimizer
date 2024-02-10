caa0c84f1af8842ec100ffa2a8ecef0ab87e0f00
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2024-02-10 10:54:50 +0200

    add statements generator calls

--- src/main/java/tiem625/anonimizer/tooling/sql/jdbc/JdbcBatchQueriesResolver.java
+++ src/main/java/tiem625/anonimizer/tooling/sql/jdbc/JdbcBatchQueriesResolver.java
@@ -13,29 +13,35 @@ import java.util.List;
 public class JdbcBatchQueriesResolver implements BatchQueriesResolver {
 
     private final DataSource dataSource;
+    private final SQLStatementGenerator sqlStatements;
 
     @Inject
     public JdbcBatchQueriesResolver(DataSource jdbcDataSource) {
         this.dataSource = jdbcDataSource;
+        this.sqlStatements = new SQLStatementGenerator(SQLStatementGeneratorConfig.builder().build());
     }
 
     @Override
     public void executeBatchCreationQuery(BatchName batchName, List<DataGenerator.DataFieldSpec> fieldSpecs) {
+        var statement = sqlStatements.createTableStatement(batchName, fieldSpecs);
         throw new UnsupportedOperationException("TODO");
     }
 
     @Override
     public boolean executeCheckBatchExistsQuery(BatchName batchName) {
+        var statement = sqlStatements.checkTableExistsStatement(batchName);
         throw new UnsupportedOperationException("TODO");
     }
 
     @Override
     public Amount executeFetchBatchSizeQuery(BatchName batchName) {
+        var statement = sqlStatements.getTableSizeStatement(batchName);
         throw new UnsupportedOperationException("TODO");
     }
 
     @Override
     public List<DataObject> executeFetchSomeBatchObjectsQuery(BatchName batchName, Amount amount) {
+        var statement = sqlStatements.fetchTableRowsStatement(batchName, amount);
         throw new UnsupportedOperationException("TODO");
     }
 }
